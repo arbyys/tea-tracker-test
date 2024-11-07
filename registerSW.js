ca0b4060dae3feaa3bb700063cfca4103e41486d
arbyys <adam.petricek@centrum.cz>
2024-11-07 09:48:34 +0100

    Fix

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/tea-tracker-test/sw.js', { scope: '/tea-tracker-test/' })})}
